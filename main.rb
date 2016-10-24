4300d2d02bb81fc191c854ca212aa6c3908e2e13
tim <tim.w@axsoftware.net>
2016-10-24 11:05:33 +0800

    add error log

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 require_relative 'lib/controller'
 
-service = Controller.new
-service.start 
+begin
+	service = Controller.new
+	service.start 
+rescue => e
+	error_log = File.new('log.txt','w')
+	error_log << e
+	error_log.close
+end
