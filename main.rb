4d2305afb1acc5b1a2edecc8123c649d1e751566
tim <tim.w@axsoftware.net>
2016-10-24 11:33:32 +0800

    'logger'

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
-require_relative 'lib/controller'
-
-if not defined?(Ocra)	
-	begin
-			service = Controller.new
-			service.start 
-	rescue => e
-		error_log = File.new('log.txt','w')
-		error_log << e
-		error_log.close
+begin
+	require_relative 'lib/controller'
+	
+	if not defined?(Ocra)	
+		service = Controller.new
+		service.start 
 	end
+rescue => e
+	error_log = File.new('log.txt','w')
+	error_log << e
+	error_log.close
 end
